a01d4481f606a5e172676924e5c23e2d83f211a1
Andries George Șerban <105744041+Geo7ge@users.noreply.github.com>
2023-03-17 22:08:55 +0200

    Add files via upload

---
+++ Biblioteca.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab2_Tema
+{
+    internal class Biblioteca
+    {
+        public static Carte CitesteCarte()
+        {
+            Console.WriteLine("Introduceti titlul cartii:");
+            string titlu = Console.ReadLine();
+
+            Console.WriteLine("Introduceti autorul cartii:");
+            string autor = Console.ReadLine();
+
+            Console.WriteLine("Introduceti anul publicarii:");
+            int anPublicatie = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti valabilitatea cartii (true/false):");
+            bool valabilitate = bool.Parse(Console.ReadLine());
+            return new Carte(titlu, autor, anPublicatie, valabilitate);
+        }
+
+        public void StergeCarte(Carte carte)
+        {
+
+        }
+
+        public void CautaCarte(string criteriu)
+        {
+
+        }
+
+        public void AfiseazaCarti()
+        {
+
+        }
+    }
+}
---
+++ Carte.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_Tema
+{
+    internal class Carte
+    {
+        public string Titlu { get; set; }
+        public string Autor { get; set; }
+        public int AnPublicatie { get; set; }
+        public bool Valabilitate { get; set; }
+
+        public Carte(string titlu, string autor, int anPublicatie, bool valabilitate)
+        {
+            Titlu = titlu;
+            Autor = autor;
+            AnPublicatie = anPublicatie;
+            Valabilitate = valabilitate;
+        }
+    }
+}
