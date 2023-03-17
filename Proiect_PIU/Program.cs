b5c1958e0040bbd4b9e2876e11e8d35ad770d1d4
Andries George Șerban <105744041+Geo7ge@users.noreply.github.com>
2023-03-17 22:10:28 +0200

    Add files via upload

---
+++ Proiect_PIU/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_Tema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Carte carte = Carte.CitesteCarte();
+        }
+    }
+}
